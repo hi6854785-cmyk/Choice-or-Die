3fd241ed9e4c0c54389d02556288fb32b82cf84a
hi6854785-cmyk <hi6854785@gmail.com>
2026-01-14 14:39:34 +0900

    Update game.js

--- game.js
+++ game.js
@@ -8,3 +8,12 @@ function checkAnswer(choice) {
   }
 }
 
+function jumpScare() {
+  const img = document.getElementById("scare");
+  img.style.display = "block";
+
+  setTimeout(() => {
+    img.style.display = "none";
+    showQuestion(); // 실패 지점부터 재개
+  }, 400);
+}
